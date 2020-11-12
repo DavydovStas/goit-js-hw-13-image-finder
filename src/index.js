50f37ddafba2041d42cc892f39c4c0e64a855ec6
unknown <vpupkin@company.com>
2020-11-12 12:35:17 +0200

    create and add responses markup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './styles.css';
 import ImagesApiService from './js/apiService';
-
+import imageCardsTemplate from './templates/image-card.hbs';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
   loadMore: document.querySelector('.load-more'),
+  imagesConatainer: document.querySelector('.gallery'),
 }
 
 const imagesApiService = new ImagesApiService();
@@ -14,13 +15,20 @@ refs.loadMore.addEventListener('click', onLoadMore)
 
 function onSearch(e) { 
   e.preventDefault();
-
+  deleteImagesMarkup();
   imagesApiService.query = e.currentTarget.elements.query.value;
   imagesApiService.resetPage();
-  imagesApiService.fetchArticles().then(hits => console.log(hits));
+  imagesApiService.fetchArticles().then(addImagesMarkup);
 }
 
 function onLoadMore() { 
-  imagesApiService.fetchArticles().then(hits => console.log(hits));
+  imagesApiService.fetchArticles().then(addImagesMarkup);
 }
 
+function addImagesMarkup(hits) { 
+  refs.imagesConatainer.insertAdjacentHTML('beforeend', imageCardsTemplate(hits))
+}
+
+function deleteImagesMarkup() { 
+  refs.imagesConatainer.innerHTML = '';
+}
